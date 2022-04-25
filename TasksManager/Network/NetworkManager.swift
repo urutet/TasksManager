b7a1382bfb1c486b2ed29902b1821cef9f20acce
LuckyBlade <91490587+LuckyBlade@users.noreply.github.com>
2022-04-25 19:59:50 +0400

    Commit for requested changes

--- TasksManager/Network/NetworkManager.swift
+++ TasksManager/Network/NetworkManager.swift
@@ -5,14 +5,16 @@ final class NetworkManager {
     
     static let instance = NetworkManager()
     private enum Constants {
-        static let baseUrl = "https://jsonplaceholder.typicode.com/"
+        static let baseUrl = "https://jsonplaceholder.typicode.com"
     }
     private enum EndPoints {
         static let endUrl = "/todos"
     }
     
-    func getData (completion: @escaping(([ToDoModel])->Void)) {
-        AF.request(Constants.baseUrl + EndPoints.endUrl).responseDecodable(of:[ToDoModel].self) {responceIn in
+    private init(){}
+    
+    func getData (completion: @escaping(([ToDo])->Void)) {
+        AF.request(Constants.baseUrl + EndPoints.endUrl).responseDecodable(of: [ToDo].self) {responceIn in
             switch responceIn.result {
             case .success(let model):
                 completion(model)
@@ -21,7 +23,5 @@ final class NetworkManager {
             }
         }
     }
-    
-    private init(){}
     
 }
--- TasksManager/Network/ToDoModel.swift
+++ TasksManager/Network/ToDoModel.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-struct ToDoModel: Codable {
-    let userId:Int
-    let Id:Int
-    let title:String
-    let completed:Bool
+struct ToDo: Codable {
+    let userId: Int
+    let Id: Int
+    let title: String
+    let completed: Bool
 }
